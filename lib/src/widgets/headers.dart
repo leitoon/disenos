92aea9ac78e6782ebc2c6fdcabb4f4769e79805a
krika mobile <krikamobile@gmail.com>
2024-03-13 17:23:28 -0500

    avances

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -77,3 +77,93 @@ class _HeaderDiagonalPainter extends CustomPainter
   }
   
 }
+
+
+class HeaderTriangular extends StatelessWidget {
+   
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container
+    (
+      height: double.infinity,
+      width: double.infinity,
+      //color: const Color(0xff615aab),
+      child: CustomPaint
+      (
+        painter: _HeaderTriangularPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderTriangularPainter extends CustomPainter
+{
+  @override
+  void paint(Canvas canvas, Size size) {
+    // TODO: implement paint
+    final paint=Paint();
+    paint.color=Color(0xff615aab);
+    paint.style=PaintingStyle.fill;
+    paint.strokeWidth=2;
+    final path=Path();
+    path.moveTo(0, 0);
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, 0);
+    //path.lineTo(0, size.height);
+    path.lineTo(0, 0);
+    //path.lineTo(0, size.height*0.5);
+    canvas.drawPath(path, paint);
+    
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+  
+}
+
+class HeaderPico extends StatelessWidget {
+   
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container
+    (
+      height: double.infinity,
+      width: double.infinity,
+      //color: const Color(0xff615aab),
+      child: CustomPaint
+      (
+        painter: _HeaderPicoPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderPicoPainter extends CustomPainter
+{
+  @override
+  void paint(Canvas canvas, Size size) {
+    // TODO: implement paint
+    final paint=Paint();
+    paint.color=Color(0xff615aab);
+    paint.style=PaintingStyle.fill;
+    paint.strokeWidth=10;
+    final path=Path();
+    path.moveTo(0, 0);
+    path.lineTo(0, size.height*0.2);
+    path.lineTo(size.width*0.5, size.height*0.28);
+    path.lineTo(size.width, size.height*0.2);
+    path.lineTo(size.width, 0);
+    canvas.drawPath(path, paint);
+    
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+  
+}
